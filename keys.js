78da3a5a94dff7cc4d2432559d2bb5c741920b5c
AustinSFleet <austinsfleet@gmail.com>
2017-09-05 20:36:55 -0500

    added everything for twitter

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'gt7AP3r0o9M3U6deW7rrv4QiE',
+  consumer_secret: 'vX5bnORFjvodIZVUP3upbwySADSqHcjw4DfowXZPHYvHeUhOei',
+  access_token_key: '905234559058538496-eV1OpvXbtvdPbbm5e5PVujC3RstMJ8Q',
+  access_token_secret: 'Zh4jDlxpqCRRx3IJvLzAlWuAXhx6aoQ3LclSB1c5LADq9',
+}
